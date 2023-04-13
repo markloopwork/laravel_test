d716849b5d54c5f2710acf3b71031da94492b83e
markloopwork <markloopwork@gmail.com>
2023-04-13 14:47:39 +0530

    change route name

--- app/Http/Controllers/API/BaseController.php
+++ app/Http/Controllers/API/BaseController.php
@@ -22,6 +22,7 @@ class BaseController extends Controller
             'data'    => $result,
             'message' => $message,
         ];
+        
         return response()->json($response, 200);
     }
 
